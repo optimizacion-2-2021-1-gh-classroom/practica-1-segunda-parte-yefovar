077511665c3aa9b6ebf76de2f96c5f23bee11765
jreyesgar93 <50057293+jreyesgar93@users.noreply.github.com>
2021-03-18 20:39:32 -0600

    armando paquete

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+ 
+setup(name = 'Simplex',
+      version = '0.1',
+      description = ' PAckage to solve Linear Programming problems using Simplex Algorithm',
+      url = '',
+      author = 'Equipo 3 MNO 2021',
+      license = 'MIT',
+      packages = find_packages(),
+      install_requires = ['numpy','pandas','nose']
+      )
+
