d118b76db24ba9b87e4ff071448619b2f73b020a
Yedam <66651590+yefovar@users.noreply.github.com>
2021-03-21 13:42:38 -0600

    Configuracion de python para pruebas unitarias
    Draft de programacion para realizar pruebas unitarias desde github actions

---
+++ src/tests/test.py
@@ -0,0 +1,27 @@
+#from __init__ import *
+
+from Simplex import *
+from scipy.optimize import linprog
+from pytest import approx
+
+#Maximization case
+
+#Minimization case
+
+c = np.array([1, 1, -4])
+
+A = np.array([[ 1,  1,  2],
+              [ 1,  1, -1],
+              [-1,  1,  1]])
+
+b = np.array([9,2,4])
+
+opt = linprog(c=c, A_ub=A, b_ub=b,
+              method="simplex")
+python_result = opt.x
+
+
+problema = Simplex(c,A,b,problem='Min')
+method_result = problema.solve()
+
+print(method_result== approx(python_result, abs=1e-8, rel=1e-8))
