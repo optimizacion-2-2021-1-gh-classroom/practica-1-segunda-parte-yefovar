0b583373ad6c21e6e3a86e652fa9b926c6fe757e
jreyesgar93 <50057293+jreyesgar93@users.noreply.github.com>
2021-05-08 14:49:32 -0500

    modulo compilado

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup, Extension
 from setuptools import find_packages
 from Cython.Build import cythonize
+import os
+
+
+
 setup(name = 'Simplex',
       version = '0.1',
       description = ' PAckage to solve Linear Programming problems using Simplex Algorithm',
@@ -8,8 +12,7 @@ setup(name = 'Simplex',
       author = 'Equipo 3 MNO 2021',
       license = 'MIT',
       packages = find_packages(),
-      install_requires = ['numpy','pandas','cython'],
-      ext_modules = cythonize("SimplexC/__init__.pyx", 
-                              compiler_directives={'language_level' : 3})
+      install_requires = ['numpy','pandas','Cython'],
+      ext_modules = cythonize("SimplexC/__init__.pyx")
       )
 
